00b5955b118f582dad9e720a04ecefc96f04dfe2
toekneechin777 <chen4714@umn.edu>
2018-12-29 03:40:36 -0600

    Add basic functionality for random roll die.

--- app/src/main/java/com/example/anthony/rngpart/BasicDice.java
+++ app/src/main/java/com/example/anthony/rngpart/BasicDice.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class BasicDice extends AppCompatActivity {
 
@@ -16,5 +20,17 @@ public class BasicDice extends AppCompatActivity {
     public void back(View v) {
         Intent returnToMenu = new Intent(this, MainMenu.class);
         startActivity(returnToMenu);
+    }
+
+    public void rng(View v) {
+        EditText minBox = (EditText) findViewById(R.id.input_min);
+        EditText maxBox = (EditText) findViewById(R.id.input_max);
+        EditText intervalBox = (EditText) findViewById(R.id.input_interval);
+        int vMin = Integer.valueOf(minBox.getText().toString());
+        int vMax = Integer.valueOf(maxBox.getText().toString());
+        double vInterval = Double.valueOf(intervalBox.getText().toString());
+        final double rngAnswer = new Random().nextInt((int) Math.round((vMax-vMin)/vInterval))*vInterval + vMin;
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText(Double.toString(rngAnswer));
     }
 }
