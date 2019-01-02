fa3b4d2eac7044576f899eb6e49e86cefbc9fb8f
toekneechin777 <chen4714@umn.edu>
2019-01-01 22:28:00 -0600

    Added Random Color Generator

--- app/src/main/java/com/example/anthony/rngpart/BasicDice.java
+++ app/src/main/java/com/example/anthony/rngpart/BasicDice.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -11,10 +13,15 @@ import java.util.Random;
 
 public class BasicDice extends AppCompatActivity {
 
+    int[] rngColors;
+    String[] rngColorNames;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_dice);
+        rngColors = getResources().getIntArray(R.array.rng_colors);
+        rngColorNames = getResources().getStringArray(R.array.rng_colors_name);
     }
 
     public void back(View v) {
@@ -28,9 +35,33 @@ public class BasicDice extends AppCompatActivity {
         EditText intervalBox = (EditText) findViewById(R.id.input_interval);
         int vMin = Integer.valueOf(minBox.getText().toString());
         int vMax = Integer.valueOf(maxBox.getText().toString());
+
+        if (vMax < vMin) {
+            showError(minBox);
+            return;
+        }
+
         double vInterval = Double.valueOf(intervalBox.getText().toString());
         final double rngAnswer = new Random().nextInt((int) Math.round((vMax-vMin)/vInterval))*vInterval + vMin;
         TextView result = (TextView) findViewById(R.id.result);
-        result.setText(Double.toString(rngAnswer));
+        result.setText("You rolled: " + Double.toString(rngAnswer));
     }
+
+
+    public void color_rng(View v) {
+        TextView colorBox = (TextView) findViewById(R.id.color);
+        int color_index = new Random().nextInt(rngColors.length);
+        int randColor = rngColors[color_index];
+        colorBox.setBackgroundColor(randColor);
+        String randColorName = rngColorNames[color_index];
+        colorBox.setText(randColorName);
+    }
+
+    private void showError(EditText et) {
+        Animation shake = AnimationUtils.loadAnimation(this,R.anim.shake);
+        et.startAnimation(shake);
+        et.setError("Min must be smaller than Max");
+    }
+
+
 }
